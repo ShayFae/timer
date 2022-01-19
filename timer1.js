bc3c555efbfff3dc10f016e5a22fde4618f729f8
ShayFae <ifrahnoor108@gmail.com>
2022-01-19 01:37:53 +0000

    Eslint fix

--- timer1.js
+++ timer1.js
@@ -5,32 +5,32 @@
 
 ///Function with user number input
 const test = function(input) {
-  if(input.length === 0 || input < 0) {
+  //Edge case check if there's no input && if it's negative it would return an empty string
+  if (input.length === 0 || input < 0) {
     return '';
-    
   }
   //Checks if input length is one
-  if(input.length === 1) {  
-     setTimeout(() => {
-     return process.stdout.write('.');
+  if (input.length === 1) {
+    setTimeout(() => {
+      return process.stdout.write('.');
     }, input * 1000);
-  } 
-    //else it loops through input index till it's no longer greater than one
-    for(let i = 0; i < input.length; i++) {
-      if(input.length > 1) {
+  }
+  //else it loops through input index till it's no longer greater than one
+  for (let i = 0; i < input.length; i++) {
+    if (input.length > 1) {
       // console.log(input[i])
-      if(input[i] > 0 ) {
-      setTimeout(() => {
-        process.stdout.write('.');
+      if (input[i] > 0) {
+        setTimeout(() => {
+          process.stdout.write('.');
         // console.log(input[i]);
-      }, input[i] * 1000);
+        }, input[i] * 1000);
       }
     }
   }
   setTimeout(() => process.stdout.write('\n'), 4000);
 };
 
-test([1,6,8])
-test([5])
+test([1,6,8]);
+test([5]);
 // test(['Cat']);
 // test([-1,-3,-5])
